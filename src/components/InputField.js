63a7ce73b849e90266e63c951db183d057826e1c
sujay <jirapuresujay@gmail.com>
2023-03-05 23:24:23 +0530

    done2

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,13 +1,14 @@
-import React ,{useRef,useState,useEffect} from 'react';
+import React ,{useRef,useState,useEffect ,forwardRef} from 'react';
 import '../styles/App.css';
 
 //complete the following using forward refs concept 
 
-const InputField=()=>{
+const InputField = forwardRef((props,ref)=>{
     return(
     <div>
       <input id="input" type="text"  ref={ref}/>
+      
     </div>
-    )
-    }
+  )
+})
 export default InputField;
